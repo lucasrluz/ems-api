3ad0e2f31a26c0f4e673c5bc738f5965a3e0244f
lucasrluz <lucasr.luzbr@gmail.com>
2023-09-01 14:30:17 -0300

    refactor: alteração para senha hash no UserModelBuilder

--- src/test/java/com/emsapi/util/UserModelBuilder.java
+++ src/test/java/com/emsapi/util/UserModelBuilder.java
@@ -4,12 +4,14 @@ import java.util.UUID;
 
 import com.emsapi.models.UserModel;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 public abstract class UserModelBuilder {
     public static UserModel createWithEmptyUserId() {
         return new UserModel("foo", "bar", "foobar@gmail.com", "123");
     }
 
     public static UserModel createWithUserId() {
-        return new UserModel(UUID.randomUUID(), "foo", "bar", "foobar@gmail.com", "123");
+        return new UserModel(UUID.randomUUID(), "foo", "bar", "foobar@gmail.com", BCrypt.withDefaults().hashToString(12, "123".toCharArray()));
     }
 }
