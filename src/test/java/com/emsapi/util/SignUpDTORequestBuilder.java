578e62955a5369ad2f25f35bb25d129ff3e2119a
lucasrluz <lucasr.luzbr@gmail.com>
2023-09-01 09:51:53 -0300

    feat: adicionado mais objetos builders com valores para os testes

--- src/test/java/com/emsapi/util/SignUpDTORequestBuilder.java
+++ src/test/java/com/emsapi/util/SignUpDTORequestBuilder.java
@@ -6,4 +6,24 @@ public abstract class SignUpDTORequestBuilder {
     public static SignUpDTORequest createWithValidData() {
         return new SignUpDTORequest("foo", "bar", "foobar@gmail.com", "123");
     }
+
+    public static SignUpDTORequest createWithEmptyFirstName() {
+        return new SignUpDTORequest("", "bar", "foobar@gmail.com", "123");
+    }
+
+    public static SignUpDTORequest createWithEmptyLastName() {
+        return new SignUpDTORequest("foo", "", "foobar@gmail.com", "123");
+    }
+
+    public static SignUpDTORequest createWithEmptyEmail() {
+        return new SignUpDTORequest("foo", "bar", "", "123");
+    }
+
+    public static SignUpDTORequest createWithInvalidEmailFormat() {
+        return new SignUpDTORequest("foo", "bar", "foobar@", "123");
+    }
+
+    public static SignUpDTORequest createWithEmpyPassword() {
+        return new SignUpDTORequest("foo", "bar", "foobar@gmail.com", "");
+    }
 }
