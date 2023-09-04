2a492da574ed2e5c5640aa52f45420a71a2f8951
lucasrluz <lucasr.luzbr@gmail.com>
2023-09-04 13:20:28 -0300

    feat: adicionado rota para busca de usuário

--- src/main/java/com/emsapi/controllers/UserController.java
+++ src/main/java/com/emsapi/controllers/UserController.java
@@ -2,11 +2,14 @@ package com.emsapi.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.emsapi.dtos.GetUserDTOResponse;
 import com.emsapi.dtos.SignUpDTORequest;
 import com.emsapi.dtos.SignUpDTOResponse;
 import com.emsapi.services.UserService;
@@ -28,5 +31,14 @@ public class UserController {
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> get(Authentication authentication) {
+        String userId = authentication.getName();
+
+        GetUserDTOResponse getUserDTOResponse = this.userService.get(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(getUserDTOResponse);
     }
 }
