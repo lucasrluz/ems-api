6041f9d6cb11712e89496125128826ac32c7d867
lucasrluz <lucasr.luzbr@gmail.com>
2023-09-16 11:11:48 -0300

    feat: adicionado CompanyRepository

---
+++ src/main/java/com/emsapi/repositories/CompanyRepository.java
@@ -0,0 +1,11 @@
+package com.emsapi.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.emsapi.models.CompanyModel;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyModel, UUID> {}
