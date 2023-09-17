485ddac42c524d260782a2eee38d0f28e7f97395
lucasrluz <lucasr.luzbr@gmail.com>
2023-09-17 20:26:38 -0300

    feat: adicionado método no CompanyRepository para busca de CompanyModel pelo UserModel

--- src/main/java/com/emsapi/repositories/CompanyRepository.java
+++ src/main/java/com/emsapi/repositories/CompanyRepository.java
@@ -1,11 +1,15 @@
 package com.emsapi.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.emsapi.models.CompanyModel;
+import com.emsapi.models.UserModel;
 
 @Repository
-public interface CompanyRepository extends JpaRepository<CompanyModel, UUID> {}
+public interface CompanyRepository extends JpaRepository<CompanyModel, UUID> {
+    List<CompanyModel> findByUserModel(UserModel userModel);
+}
