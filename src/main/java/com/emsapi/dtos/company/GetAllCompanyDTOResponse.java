5a4fe704438d9a53367c5e0d950ba950306f3bdf
lucasrluz <lucasr.luzbr@gmail.com>
2023-09-17 20:25:57 -0300

    feat: adicionado GetAllCompanyDTOResponse

---
+++ src/main/java/com/emsapi/dtos/company/GetAllCompanyDTOResponse.java
@@ -0,0 +1,37 @@
+package com.emsapi.dtos.company;
+
+public class GetAllCompanyDTOResponse {
+    private String companyId;
+    private String name;
+    private String description;
+
+    public GetAllCompanyDTOResponse(String companyId, String name, String description) {
+        this.companyId = companyId;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
