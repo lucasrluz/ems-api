129517235f330467001cb517acccaf8b91883dda
lucasrluz <lucasr.luzbr@gmail.com>
2023-09-19 15:19:46 -0300

    feat: adicionado rota/método para uscar companys do usuário

--- src/main/java/com/emsapi/controllers/CompanyController.java
+++ src/main/java/com/emsapi/controllers/CompanyController.java
@@ -1,12 +1,17 @@
 package com.emsapi.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.emsapi.dtos.company.GetAllCompanyDTOResponse;
 import com.emsapi.dtos.company.SaveCompanyDTORequest;
 import com.emsapi.dtos.company.SaveCompanyDTOResponse;
 import com.emsapi.services.CompanyService;
@@ -31,5 +36,14 @@ public class CompanyController {
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAll(Authentication authentication) {
+        String userId = authentication.getName();
+
+        List<GetAllCompanyDTOResponse> getAllCompanyDTOResponse = this.companyService.getAll(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(getAllCompanyDTOResponse);
     }
 }
