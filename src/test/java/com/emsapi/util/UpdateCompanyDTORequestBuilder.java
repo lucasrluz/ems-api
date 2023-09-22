88acdf5656b0c4dfe7544b7b686860352270779e
lucasrluz <lucasr.luzbr@gmail.com>
2023-09-22 09:19:22 -0300

    feat: adicionado Builder para UpdateCompanyDTORequest

---
+++ src/test/java/com/emsapi/util/UpdateCompanyDTORequestBuilder.java
@@ -0,0 +1,9 @@
+package com.emsapi.util;
+
+import com.emsapi.dtos.company.UpdateCompanyDTORequest;
+
+public abstract class UpdateCompanyDTORequestBuilder {
+    public static UpdateCompanyDTORequest createWithValidData() {
+        return new UpdateCompanyDTORequest("bar", "foo");
+    }
+}
