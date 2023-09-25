3caa1ffc64dc263500dc5ae7ae9869d94f3d570d
lucasrluz <lucasr.luzbr@gmail.com>
2023-09-25 10:19:26 -0300

    feat: adicionado Exception quando RoleDomain for inválida

---
+++ src/main/java/com/emsapi/domains/util/InvalidRoleDomainException.java
@@ -0,0 +1,7 @@
+package com.emsapi.domains.util;
+
+public class InvalidRoleDomainException extends Exception {
+    public InvalidRoleDomainException(String message) {
+        super(message);
+    }
+}
