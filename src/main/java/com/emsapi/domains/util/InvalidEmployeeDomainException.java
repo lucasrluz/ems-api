6a18d3cc855fb411edf6d309488612e04bfc380e
lucasrluz <lucasr.luzbr@gmail.com>
2023-09-25 09:42:19 -0300

    feat: adicionado Exception quando EmployeeDomain for inválido

---
+++ src/main/java/com/emsapi/domains/util/InvalidEmployeeDomainException.java
@@ -0,0 +1,7 @@
+package com.emsapi.domains.util;
+
+public class InvalidEmployeeDomainException extends Exception {
+    public InvalidEmployeeDomainException(String message) {
+        super(message);
+    }
+}
