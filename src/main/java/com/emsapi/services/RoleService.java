c34ed5703a73e4967071ac610075b2a46d9b3837
lucasrluz <lucasr.luzbr@gmail.com>
2023-09-27 10:38:36 -0300

    feat: adicionado método no RoleService para buscar Role pelo RoleId

--- src/main/java/com/emsapi/services/RoleService.java
+++ src/main/java/com/emsapi/services/RoleService.java
@@ -3,17 +3,21 @@ package com.emsapi.services;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+
 
 import org.springframework.stereotype.Service;
 
 import com.emsapi.domains.RoleDomain;
 import com.emsapi.domains.util.InvalidRoleDomainException;
 import com.emsapi.dtos.role.GetAllRoleDTOResponse;
+import com.emsapi.dtos.role.GetRoleDTOResponse;
 import com.emsapi.dtos.role.SaveRoleDTORequest;
 import com.emsapi.dtos.role.SaveRoleDTOResponse;
 import com.emsapi.models.RoleModel;
 import com.emsapi.repositories.RoleRepository;
 import com.emsapi.services.util.NameAlreadyRegisteredException;
+import com.emsapi.services.util.RoleNotFoundException;
 
 @Service
 public class RoleService {
@@ -50,5 +54,18 @@ public class RoleService {
         });
 
         return getAllRoleDTOResponses;
+    }
+
+    public GetRoleDTOResponse get(String roleId) throws RoleNotFoundException {
+        Optional<RoleModel> findRoleByRoleId = this.roleRepository.findById(UUID.fromString(roleId));
+
+        if (findRoleByRoleId.isEmpty()) {
+            throw new RoleNotFoundException();
+        }
+
+        return new GetRoleDTOResponse(
+            findRoleByRoleId.get().getRoleId().toString(),
+            findRoleByRoleId.get().getName()
+        );
     }
 }
---
+++ src/test/java/com/emsapi/unit/services/role/RoleServiceGetTests.java
@@ -0,0 +1,56 @@
+package com.emsapi.unit.services.role;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import com.emsapi.dtos.role.GetRoleDTOResponse;
+import com.emsapi.models.RoleModel;
+import com.emsapi.repositories.RoleRepository;
+import com.emsapi.services.RoleService;
+import com.emsapi.services.util.NameAlreadyRegisteredException;
+import com.emsapi.services.util.RoleNotFoundException;
+import com.emsapi.util.RoleModelBuilder;
+
+@ExtendWith(SpringExtension.class)
+public class RoleServiceGetTests {
+    @InjectMocks
+    private RoleService roleService;
+
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Test
+    public void retornaRole() throws Exception {
+        // Mocks
+        Optional<RoleModel> roleModelOptionalMock = Optional.of(RoleModelBuilder.createWithRoleId());
+        BDDMockito.when(this.roleRepository.findById(ArgumentMatchers.any())).thenReturn(roleModelOptionalMock);
+
+        // Test
+        GetRoleDTOResponse getRoleDTOResponse = this.roleService.get(roleModelOptionalMock.get().getRoleId().toString());
+
+        assertThat(getRoleDTOResponse.getRoleId()).isEqualTo(roleModelOptionalMock.get().getRoleId().toString());
+        assertThat(getRoleDTOResponse.getName()).isEqualTo(roleModelOptionalMock.get().getName());
+    }
+
+    @Test
+    public void retornaException_RoleNaoEncontrada_RoleNaoCadastrada() {
+        // Mocks
+        BDDMockito.when(this.roleRepository.findById(ArgumentMatchers.any())).thenReturn(Optional.empty());
+
+        // Test
+        assertThatExceptionOfType(RoleNotFoundException.class)
+        .isThrownBy(() -> this.roleService.get(UUID.randomUUID().toString()))
+        .withMessage("Role not found");
+    }
+}
