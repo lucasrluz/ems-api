9b541fe6cebd67cee39f70a92caa898f19182c0a
lucasrluz <lucasr.luzbr@gmail.com>
2023-10-04 10:42:44 -0300

    feat: adicionado método que retorna lista no EmployeeModelBuilder

--- src/test/java/com/emsapi/util/EmployeeModelBuilder.java
+++ src/test/java/com/emsapi/util/EmployeeModelBuilder.java
@@ -1,5 +1,7 @@
 package com.emsapi.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import com.emsapi.models.CompanyModel;
@@ -18,5 +20,44 @@ public abstract class EmployeeModelBuilder {
             roleModel,
             companyModel
         );
+    }
+
+    public static List<EmployeeModel> createListWithEmployeeId(RoleModel roleModel, CompanyModel companyModel) {
+        List<EmployeeModel> employeeModels = new ArrayList<EmployeeModel>();
+
+        employeeModels.add(new EmployeeModel(
+            UUID.randomUUID(),
+            "foo",
+            "bar",
+            "21",
+            "3828 Piermont Dr, Albuquerque, NM",
+            "foobar@gmail.com",
+            roleModel,
+            companyModel
+        ));
+
+        employeeModels.add(new EmployeeModel(
+            UUID.randomUUID(),
+            "bar",
+            "foo",
+            "21",
+            "3828 Piermont Dr, Albuquerque, NM",
+            "barfoo@gmail.com",
+            roleModel,
+            companyModel
+        ));
+
+        employeeModels.add(new EmployeeModel(
+            UUID.randomUUID(),
+            "foo",
+            "baz",
+            "21",
+            "3828 Piermont Dr, Albuquerque, NM",
+            "foobaz@gmail.com",
+            roleModel,
+            companyModel
+        ));
+
+        return employeeModels;
     }
 }
