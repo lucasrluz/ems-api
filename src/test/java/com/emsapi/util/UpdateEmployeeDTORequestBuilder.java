007918906b964ddb7c0972bd0e3ed148e5580071
lucasrluz <lucasr.luz42@gmail.com>
2023-11-09 16:03:49 -0300

    feat: adicionado novos dados para testes no UpdateEmployeeDTORequestBuilder

--- src/test/java/com/emsapi/util/UpdateEmployeeDTORequestBuilder.java
+++ src/test/java/com/emsapi/util/UpdateEmployeeDTORequestBuilder.java
@@ -6,4 +6,28 @@ public abstract class UpdateEmployeeDTORequestBuilder {
     public static UpdateEmployeeDTORequest createWithValidData(String roleId, String companyId) {
         return new UpdateEmployeeDTORequest("bar", "foo", "22", "9809 Margo Street", companyId, "barfoo@gmail.com", roleId);
     }
+
+	public static UpdateEmployeeDTORequest createWithEmptyFirstName(String roleId, String companyId) {
+		return new UpdateEmployeeDTORequest("", "foo", "22", "9809 Margo Street", companyId, "barfoo@gmail.com", roleId);
+	}
+
+	public static UpdateEmployeeDTORequest createWithEmptyLastName(String roleId, String companyId) {
+		return new UpdateEmployeeDTORequest("bar", "", "22", "9809 Margo Street", companyId, "barfoo@gmail.com", roleId);
+	}
+
+	public static UpdateEmployeeDTORequest createWithEmptyAge(String roleId, String companyId) {
+		return new UpdateEmployeeDTORequest("bar", "foo", "", "9809 Margo Street", companyId, "barfoo@gmail.com", roleId);
+	}
+
+	public static UpdateEmployeeDTORequest createWithEmptyAddress(String roleId, String companyId) {
+		return new UpdateEmployeeDTORequest("bar", "foo", "22", "", companyId, "barfoo@gmail.com", roleId);
+	}
+
+	public static UpdateEmployeeDTORequest createWithEmptyEmail(String roleId, String companyId) {
+		return new UpdateEmployeeDTORequest("bar", "foo", "22", "9809 Margo Street", companyId, "", roleId);
+	}
+
+	public static UpdateEmployeeDTORequest createWithInvalidEmailFormat(String roleId, String companyId) {
+		return new UpdateEmployeeDTORequest("bar", "foo", "22", "9809 Margo Street", companyId, "@gmail.com", roleId);
+	}
 }
